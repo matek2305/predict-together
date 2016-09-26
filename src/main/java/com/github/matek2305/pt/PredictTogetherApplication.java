35681d38cc6790d68e9552d4a7131815adfbf8c1
Mateusz Urbański <matek2305@gmail.com>
2016-09-26 20:57:25 +0200

    added base dependencies

---
+++ src/main/java/com/github/matek2305/pt/PredictTogetherApplication.java
@@ -0,0 +1,15 @@
+package com.github.matek2305.pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mateusz Urbański <matek2305@gmail.com>
+ */
+@SpringBootApplication
+public class PredictTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PredictTogetherApplication.class, args);
+    }
+}
